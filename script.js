6bfc66fd373661a20d098b4a3defe7c82dc13496
schanaizasilva-cmyk <schanaiza.silva@escola.pr.gov.br>
2026-09-18 08:49:48 -0300

    alterei o index

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtir = false;
+    botaoCurtir.addEventLiistener("clik",curtir);
+    funcition curtir (){
+        const contador = botaoCurtir.querySelector("span");
+        if(curtiu === false){
+            contador.textContent++;
+            curtiu = true;}
+            else{
+                contador.textContent--;
+                curtiu = false;
+            }
 
